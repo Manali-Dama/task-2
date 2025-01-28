5cbc9f5e03a14c616489e5a4c6b4aed95c6c09d1
Manali-Dama <manali.d@medkart.in>
2025-01-28 08:18:58 +0530

    Update InputJson.js

--- json-parser/src/app/components/InputJson.js
+++ json-parser/src/app/components/InputJson.js
@@ -1,43 +1,74 @@
-"use client"
+import { useDispatch, useSelector } from 'react-redux';
+import { setInputJson, parseJson } from '../store/jsonSlice';
 
-import { useState } from "react"
-import { setInputJson, setParsedJson, setError} from "../redux/slice";
-import { useDispatch , useSelector } from "react-redux";
+const InputJsonComponent = () => {
+  const dispatch = useDispatch();
+  const inputJson = useSelector((state) => state.json.inputJson);
 
-export default function InputJson(){
+  const handleChange = (e) => {
+    dispatch(setInputJson(e.target.value));
+  };
 
-    // const [data,setData]=useState("");
+  const handleParse = () => {
+    dispatch(parseJson());
+  };
 
-    const dispatch= useDispatch();
-    const { inputJson, parsedJson, error } = useSelector((state) => state.jsonSlice);
+  return (
+    <div className="input-json">
+      <textarea
+        value={inputJson}
+        onChange={handleChange}
+        placeholder="Enter stringified JSON here"
+      />
+      <button onClick={handleParse}>Validate & Parse</button>
+    </div>
+  );
+};
 
-    const handleInputChange = (e) => {
-        dispatch(setInputJson(e.target.value));
-      };
-
-      const handleParseJson = () => {
-        try {
-          // Try to parse the JSON
-          const parsed = JSON.parse(inputJson);
-          dispatch(setParsedJson(parsed)); // If successful, update the parsed JSON state
-        } catch (e) {
-          dispatch(setError('Invalid JSON format. Please check your input.'));
-        }
-      };
+export default InputJsonComponent;
 
 
-    // const JsonView=()=>{
-    //    dispatch(setInputJson(data))
 
-    // }
+// "use client"
 
-    return(
-        <div className="input-json">
-           <h3> input json file</h3>
-           <textarea type="text" 
-           onChange={handleInputChange}
-           placeholder="add stringified json file" /><br/ >
-           <button onClick={handleParseJson}>convert</button>
-        </div>
-    )
-}
+// import { useState } from "react"
+// import { setInputJson, setParsedJson, setError} from "../redux/slice";
+// import { useDispatch , useSelector } from "react-redux";
+
+// export default function InputJson(){
+
+//     // const [data,setData]=useState("");
+
+//     const dispatch= useDispatch();
+//     const { inputJson, parsedJson, error } = useSelector((state) => state.jsonSlice);
+
+//     const handleInputChange = (e) => {
+//         dispatch(setInputJson(e.target.value));
+//       };
+
+//       const handleParseJson = () => {
+//         try {
+//           // Try to parse the JSON
+//           const parsed = JSON.parse(inputJson);
+//           dispatch(setParsedJson(parsed)); // If successful, update the parsed JSON state
+//         } catch (e) {
+//           dispatch(setError('Invalid JSON format. Please check your input.'));
+//         }
+//       };
+
+
+//     // const JsonView=()=>{
+//     //    dispatch(setInputJson(data))
+
+//     // }
+
+//     return(
+//         <div className="input-json">
+//            <h3> input json file</h3>
+//            <textarea type="text" 
+//            onChange={handleInputChange}
+//            placeholder="add stringified json file" /><br/ >
+//            <button onClick={handleParseJson}>convert</button>
+//         </div>
+//     )
+// }
