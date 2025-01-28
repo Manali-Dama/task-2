1e29526613212f238022384d8ff62bb82c11bc2b
Manali-Dama <manali.d@medkart.in>
2025-01-28 08:19:52 +0530

    Update DisplayJson.js

--- json-parser/src/app/components/DisplayJson.js
+++ json-parser/src/app/components/DisplayJson.js
@@ -1,31 +1,51 @@
-"use client"
+import { useSelector } from 'react-redux';
 
-import { useSelector } from "react-redux"
-import { setParsedJson,setError } from '../redux/slice'
+const DisplayJsonComponent = () => {
+  const { parsedJson, error } = useSelector((state) => state.json);
 
-export default function DisplayJson(){
+  return (
+    <div className="display-json">
+      {error ? (
+        <p className="error">{error}</p>
+      ) : (
+        <pre>{parsedJson ? JSON.stringify(parsedJson, null, 2) : 'Parsed JSON will appear here'}</pre>
+      )}
+    </div>
+  );
+};
 
-    const jsonFile = useSelector((data)=>data.inputJson);
+export default DisplayJsonComponent;
+
+
+
+// "use client"
+
+// import { useSelector } from "react-redux"
+// import { setParsedJson,setError } from '../redux/slice'
+
+// export default function DisplayJson(){
+
+//     const jsonFile = useSelector((data)=>data.inputJson);
     
-//   const handleParseJson = () => {
-//     try {
-//       // Try to parse the JSON
-//       const parsed = JSON.parse(jsonFile);
-//       console.log("lpg parse ",parsed);
-//       dispatch(setParsedJson(parsed)); // If successful, update the parsed JSON state
-//     } catch (e) {
-//       dispatch(setError('Invalid JSON format. Please check your input.'));
-//     }
-//   };
+// //   const handleParseJson = () => {
+// //     try {
+// //       // Try to parse the JSON
+// //       const parsed = JSON.parse(jsonFile);
+// //       console.log("lpg parse ",parsed);
+// //       dispatch(setParsedJson(parsed)); // If successful, update the parsed JSON state
+// //     } catch (e) {
+// //       dispatch(setError('Invalid JSON format. Please check your input.'));
+// //     }
+// //   };
 
-     console.log(jsonFile);
+//      console.log(jsonFile);
 
-    return(
-        <div className="display-json">
-           <h3> returning json file</h3>
-           {
-                jsonFile
-           }
-        </div>
-    )
-}
+//     return(
+//         <div className="display-json">
+//            <h3> returning json file</h3>
+//            {
+//                 jsonFile
+//            }
+//         </div>
+//     )
+// }
